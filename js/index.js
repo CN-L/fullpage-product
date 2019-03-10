baafe0954912ca96eee7db581f13d23d0331c6c5
李小亮 <1396595541@qq.com>
2019-03-10 17:04:31 +0800

    搜索框动画完成，使用fullpage插件提供的api  onLeave触发动画
    当下一屏为2且为下滑时，触发动画，使用jquery中的animate写出动画效果，因为搜索框出现后才显示”沙发“两个字，因此需要将沙发两个字放在搜索框动画后得分回调函数中

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 $(function(){
     $('#dowebok').fullpage({
         "navigation":"true",
-        "css3":"true"
+        "css3":"true",
+        // fullpage插件提供的api
+        onLeave(index,nextIndex, direction){
+            // 当下一屏为2且向下滑时
+            if(nextIndex == 2 && direction == "down"){
+                // 搜索框动画,结束后显示"沙发"两个字,因为将"沙发"动画放在搜索框后
+                $('.section2').find('.e3').animate({
+                    marginLeft:"-110",
+                    opacity:1
+                },1000,'linear',function(){
+                  $('.section2').find('.e4').animate({
+                      opacity: 1
+                  },1000,'linear')
+                }
+                );
+               
+            }
+        }
     });
 });
